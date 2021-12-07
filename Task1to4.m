02183f06cf149c5e505488d345f1881f8ed04a63
LazyCrazyTom <tclare50@gmail.com>
2021-12-07 12:08:55 +0000

    made a start

--- Task1to4.m
+++ Task1to4.m
@@ -1,19 +1,25 @@
 clear; close all;
+pkg load image;
 
 % Task 1: Pre-processing -----------------------
 % Step-1: Load input image
 I = imread('IMG_01.png');
-figure, imshow(I)
+imshow(I);
 
 % Step-2: Covert image to grayscale
 I_gray = rgb2gray(I);
-figure, imshow(I_gray)
+imshow(I_gray);
 
 % Step-3: Rescale image
+I_sized = imresize(I_gray, [512, NaN]);
+imshow(I_sized);
 
 % Step-4: Produce histogram before enhancing
+imhist(I_sized, 10);
 
 % Step-5: Enhance image before binarisation
+I_enhanced = contrast(I_sized);
+imshow(I_enhanced);
 
 % Step-6: Histogram after enhancement
 
