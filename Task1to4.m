daad22480cfc3bce4b703fe7ef2675effa8731c8
LazyCrazyTom <tclare50@gmail.com>
2022-01-10 17:30:19 +0000

    edge and segments

--- Task1to4.m
+++ Task1to4.m
@@ -2,7 +2,7 @@ clear; close all;
 
 % Task 1: Pre-processing -----------------------
 % Step-1: Load input image
-I = imread('IMG_04.png');
+I = imread('IMG_10.png');
 imshow(I);
 
 % Step-2: Covert image to grayscale
@@ -14,12 +14,10 @@ I = imresize(I, [512, NaN]);
 %imshow(I);
 
 % Step-4: Produce histogram before enhancing
-imhist(I);
+%imhist(I);
 
 % Step-5: Enhance image before binarisation
-%I_histeq = histeq(I);
-I_filt = medfilt2(I, [5,5]);
-%imshow(I);
+I = medfilt2(I, [5,5]);
 
 
 % Step-6: Histogram after enhancement
@@ -28,20 +26,26 @@ I_filt = medfilt2(I, [5,5]);
 % Step-7: Image Binarisation
 % Use Otsu's method
 G = graythresh(I);
-I = imbinarize(I, G);
-
-G = graythresh(I_filt);
-I_filt = imbinarize(I_filt, G);
+I_bin = imbinarize(I, G);
 
 
 % extra step to open and erode to further reduce noise
-I = bwareaopen(I, 400, 4);
-I_filt = bwareaopen(I_filt, 400, 4);
+I_area = bwareaopen(I_bin, 400, 4);
 
-imshowpair(I, I_filt, 'montage');
+showpair(I_bin, I_area, "montage");
 
 % Task 2: Edge detection ------------------------
+I = medfilt2(I, [12,12]);
+I = imadjust(I, [], [], 3);
+imshow(I);
+
+I = edge(I, 'log');
+
+imshow(I);
 
 % Task 3: Simple segmentation --------------------
+mask = zeros(size(I));
+mask(25:end-25,25:end-25) = 1;
+I = activecontour(I, mask, 500);
 
 % Task 4: Object Recognition --------------------
