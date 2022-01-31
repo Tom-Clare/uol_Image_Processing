583ffb9378e5bb68d020f76d68728f151c45521c
LazyCrazyTom <tclare50@gmail.com>
2022-01-31 16:48:40 +0000

    confused :)

--- Task5to6.m
+++ Task5to6.m
@@ -4,48 +4,67 @@ image_num = "09";
 Im = imread("IMG_" + image_num + ".png");
 I = rgb2gray(Im);
 
-%I = adapthisteq(I);
-%I = imadjust(I, [], [], 0.16);
-
-I = medfilt2(I);
+I = medfilt2(I, [5,5]);
 
 T = graythresh(I);
 I = imbinarize(I, T);
 
 I = bwareaopen(I, 150);
 
-I_A = imclearborder(I); % get objects not attached to border
-I_A = imfill(I_A, "holes"); % fill in holes
-SE = strel("disk", 10, 0);
-I_A = imopen(I_A, SE); % think more about this
+I_inside = imclearborder(I);
+I_outside = I & ~I_inside;
 
-I_B = I & ~I_A; % get only border-touching objects
-I_B = imopen(I_B, SE);
-I_B = bwconvhull(I_B, "objects"); % and get their outline via bwconvhull()
+%find circular objects (differenciate between red blood cells and bacteria)
+I_inside_labelled = bwlabel(I_inside); % label each region
+properties = regionprops(I_inside_labelled, "Circularity"); % get circularity of regions
+mask = [properties.Circularity] > 0.3; % only regions with over X circularity rating
+I_inside_circles = ismember(I_inside_labelled, find(mask)); % match circular onjects to those in original image
 
-I_C = I_A + I_B; % add border shapes to centre shapes
+% fill inside circles
+I_inside_circles = imfill(I_inside_circles, "holes"); % fill in holes
 
-I_bact = I - I_C;
+I_out_labelled = bwlabel(I_outside);
+properties2 = regionprops(I_out_labelled, "Circularity"); % get circularity of regions
+mask2 = [properties2.Circularity] > 0.1; % only regions with over X circularity rating
+I_out_circles = ismember(I_out_labelled, find(mask2)); % match circular onjects to those in original image
 
-SE = strel("disk", 3);
-I_C = imerode(I_C, SE);
+I_out_circles = bwconvhull(I_out_circles, "objects"); % and get their outline via bwconvhull()
 
-I_C2 = imfill(I_bact, "holes");
-I_C2 = bwareaopen(I_C2, 150);
-
-I = I_C + I_C2;
-
-I_labelled = bwlabel(I); % label each region
-properties = regionprops(I_labelled, "Circularity"); % get circularity of regions
-mask = [properties.Circularity] > 0.6; % only regions with over X circularity rating
-I_circles = ismember(I_labelled, find(mask)); % match circular onjects to those in original image
+I_circles = I_inside_circles + I_out_circles; % inner and outer circles
 I_noncircles = I & ~I_circles;
 
-I_circles = bwareaopen(I_circles, 1000, 4);
+imshow(I_circles);
 
-I_circles = imerode(I_circles, 1);
+% fill in circular objects
 
-I = I_circles + I_noncircles;
+%I_A = imfill(I_inside_circles, "holes"); % fill in holes
+%SE = strel("disk", 10, 0);
+%I_A = imopen(I_A, SE); % think more about this
+
+%I_B = I & ~I_A; % get only border-touching objects
+%I_B = imopen(I_B, SE);
+%I_B = bwconvhull(I_B, "objects"); % and get their outline via bwconvhull()
+
+%I_circles = I_A + I_B; % add border circles to centre circles
+
+%I_bact = I - I_circles;
+
+%SE = strel("disk", 3); % unsure what this is doing
+%I_circles = imerode(I_circles, SE);
+
+
+%I_C2 = bwareaopen(I_bact, 150); %remove small bits of (noncircular) noise
+
+%I = I_circles + I_C2;
+
+
+
+%I_circles = bwareaopen(I_circles, 1000, 4);
+
+%I_circles = imerode(I_circles, 1);
+
+%I = I_circles + I_noncircles;
+%composite = cat(3, I_circles, I_bact, I_bact);
 
 % Task 6: Performance evaluation -----------------
 % Step 1: Load ground truth data
@@ -57,4 +76,4 @@ GT = rgb2gray(GT); % grayscale image for label2rgb function
 L_GT = label2rgb(GT, 'prism','k','shuffle');
 %figure, imshow(L_GT);
 
-imshowpair(L_GT, I, "montage");
+%imshowpair(L_GT, im2uint8(composite), "montage");
